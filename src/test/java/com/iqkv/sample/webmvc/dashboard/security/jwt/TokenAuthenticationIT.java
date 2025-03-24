51660a60526df197e81f36a8fee341d919319a3d
Dima Denisenko <1253839+dmitriidenisenko@users.noreply.github.com>
2025-03-24 11:50:06 +0200

    fix: nested paths for resolving app settings

--- src/test/java/com/iqkv/sample/webmvc/dashboard/security/jwt/TokenAuthenticationIT.java
+++ src/test/java/com/iqkv/sample/webmvc/dashboard/security/jwt/TokenAuthenticationIT.java
@@ -38,7 +38,7 @@ class TokenAuthenticationIT {
   @Autowired
   private MockMvc mvc;
 
-  @Value("${iqkv.security.authentication.jwt.base64-secret}")
+  @Value("${application-settings.security.authentication.jwt.base64-secret}")
   private String jwtKey;
 
   @Test
--- src/test/java/com/iqkv/sample/webmvc/dashboard/security/jwt/TokenAuthenticationSecurityMetersIT.java
+++ src/test/java/com/iqkv/sample/webmvc/dashboard/security/jwt/TokenAuthenticationSecurityMetersIT.java
@@ -45,7 +45,7 @@ class TokenAuthenticationSecurityMetersIT {
   @Autowired
   private MockMvc mvc;
 
-  @Value("${iqkv.security.authentication.jwt.base64-secret}")
+  @Value("${application-settings.security.authentication.jwt.base64-secret}")
   private String jwtKey;
 
   @Autowired
