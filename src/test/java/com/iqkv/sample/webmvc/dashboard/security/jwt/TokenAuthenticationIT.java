384b60349ddf64b977ad862b77e1f2283cfd1e92
Dima Denisenko <1253839+dimdnk@users.noreply.github.com>
2025-10-07 11:21:53 +0300

    fix: package name

--- src/test/java/com/iqkv/sample/webmvc/dashboard/security/jwt/TokenAuthenticationIT.java
+++ src/test/java/com/iqkv/sample/webmvc/dashboard/security/jwt/TokenAuthenticationIT.java
@@ -14,13 +14,13 @@
  * limitations under the License.
  */
 
-package com.iqkv.boot.security.jwt;
+package com.iqkv.sample.webmvc.dashboard.security.jwt;
 
-import static com.iqkv.boot.security.jwt.JwtAuthenticationTestUtils.BEARER;
-import static com.iqkv.boot.security.jwt.JwtAuthenticationTestUtils.createExpiredToken;
-import static com.iqkv.boot.security.jwt.JwtAuthenticationTestUtils.createSignedInvalidJwt;
-import static com.iqkv.boot.security.jwt.JwtAuthenticationTestUtils.createTokenWithDifferentSignature;
-import static com.iqkv.boot.security.jwt.JwtAuthenticationTestUtils.createValidToken;
+import static com.iqkv.sample.webmvc.dashboard.security.jwt.JwtAuthenticationTestUtils.BEARER;
+import static com.iqkv.sample.webmvc.dashboard.security.jwt.JwtAuthenticationTestUtils.createExpiredToken;
+import static com.iqkv.sample.webmvc.dashboard.security.jwt.JwtAuthenticationTestUtils.createSignedInvalidJwt;
+import static com.iqkv.sample.webmvc.dashboard.security.jwt.JwtAuthenticationTestUtils.createTokenWithDifferentSignature;
+import static com.iqkv.sample.webmvc.dashboard.security.jwt.JwtAuthenticationTestUtils.createValidToken;
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
--- src/test/java/com/iqkv/sample/webmvc/dashboard/security/jwt/TokenAuthenticationSecurityMetersIT.java
+++ src/test/java/com/iqkv/sample/webmvc/dashboard/security/jwt/TokenAuthenticationSecurityMetersIT.java
@@ -14,14 +14,14 @@
  * limitations under the License.
  */
 
-package com.iqkv.boot.security.jwt;
+package com.iqkv.sample.webmvc.dashboard.security.jwt;
 
-import static com.iqkv.boot.security.jwt.JwtAuthenticationTestUtils.BEARER;
-import static com.iqkv.boot.security.jwt.JwtAuthenticationTestUtils.createExpiredToken;
-import static com.iqkv.boot.security.jwt.JwtAuthenticationTestUtils.createInvalidToken;
-import static com.iqkv.boot.security.jwt.JwtAuthenticationTestUtils.createSignedInvalidJwt;
-import static com.iqkv.boot.security.jwt.JwtAuthenticationTestUtils.createTokenWithDifferentSignature;
-import static com.iqkv.boot.security.jwt.JwtAuthenticationTestUtils.createValidToken;
+import static com.iqkv.sample.webmvc.dashboard.security.jwt.JwtAuthenticationTestUtils.BEARER;
+import static com.iqkv.sample.webmvc.dashboard.security.jwt.JwtAuthenticationTestUtils.createExpiredToken;
+import static com.iqkv.sample.webmvc.dashboard.security.jwt.JwtAuthenticationTestUtils.createInvalidToken;
+import static com.iqkv.sample.webmvc.dashboard.security.jwt.JwtAuthenticationTestUtils.createSignedInvalidJwt;
+import static com.iqkv.sample.webmvc.dashboard.security.jwt.JwtAuthenticationTestUtils.createTokenWithDifferentSignature;
+import static com.iqkv.sample.webmvc.dashboard.security.jwt.JwtAuthenticationTestUtils.createValidToken;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 
